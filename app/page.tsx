1cdda09bfbdf2f7be32599f5b6aa8f92ef29e3e9
Sandra Herbert <sandraherbert50.sh@gmail.com>
2026-03-28 23:45:46 +0100

    FRONTEND-SH: remove new preview box and keep original preview section

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ import {
   Bot,
   Layers3,
 } from "lucide-react";
-import PreviewBox from "./components/PreviewBox";
 import ExploreWorldsCard from "./components/ExploreWorldsCard";
 import ProofWithoutScreenshots from "./components/ProofWithoutScreenshots";
 import ContinueSection from "./components/ContinueSection";
@@ -563,8 +562,6 @@ export default function HomePage() {
         </section>
 
         <ExploreWorldsCard />
-
-        <PreviewBox />
 
         <section id="environments" className="mt-10 sm:mt-12">
           <div className="text-xs font-semibold uppercase tracking-[0.18em] text-white/65">
